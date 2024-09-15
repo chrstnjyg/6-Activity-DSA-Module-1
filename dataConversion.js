1b4f0d43b47b4e53e4b47c6f776f9ac34bd9e5cc
Christine <guerrerochristinejoy15@gmail.com>
2024-09-15 16:32:49 +0800

    Initial commit

---
+++ dataConversion.js
@@ -0,0 +1,38 @@
+// 1. Convert a String to a Number
+let price = '200';
+
+// Using parseInt()
+price1 = parseInt(price);
+console.log(price1);
+console.log(typeof price1);
+
+// Using Number()
+price2 = Number(price);
+console.log(price2);
+console.log(typeof price2);
+
+// Using + operator
+price3 = +price;
+console.log(price3);
+console.log(typeof price3);
+
+// 2. Convert a Number to a String
+let distance = 150;
+
+// Using .toString()
+distance1 = distance.toString();
+console.log(distance1);
+console.log(typeof distance1);
+
+// Using String()
+distance2 = String(distance);
+console.log(distance2);
+console.log(typeof distance2);
+
+// 3. Convert a String to a Decimal Number
+let weight = "72.5";
+
+// Using parseFloat()
+weight = parseFloat(weight);
+console.log(weight);
+console.log(typeof weight);
